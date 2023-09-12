883c932c09e6b928c97e3f9361c8f6452cb762a6
Hector Manuel Barrios Barrios <hector.barrios@realitat.com.mx>
2023-09-12 07:07:34 -0600

    modifcar el testbench de suma con 8 bits

--- sum_8bits_08092023/testbench.vhdl
+++ sum_8bits_08092023/testbench.vhdl
@@ -7,10 +7,11 @@ end testbench;
 
 architecture tb_architecture of testbench is
     -- Señales para las entradas y salidas del testbench
-    signal a, b: std_logic_vector(7 downto 0);
-    signal cin: std_logic;
-    signal cout: std_logic;
-    signal q: std_logic_vector(7 downto 0);
+    signal a_tb: std_logic_vector(7 downto 0) := "00000000";
+	signal b_tb: std_logic_vector(7 downto 0) := "00000000";
+    signal cin_tb: std_logic := '0';
+    signal cout_tb: std_logic := '0';
+    signal q_tb: std_logic_vector(7 downto 0);
 
     -- Instancia del diseño bajo prueba
     component sum_8bits_08092023
@@ -25,13 +26,13 @@ architecture tb_architecture of testbench is
 
 begin
     -- Instancia del diseño bajo prueba
-    UUT: sum_8bits_08092023
+    uut: sum_8bits_08092023
         port map (
-            a => a,
-            b => b,
-            cin => cin,
-            cout => cout,
-            q => q
+            a => a_tb,
+            b => b_tb,
+            cin => cin_tb,
+            cout => cout_tb,
+            q => q_tb
         );
 
     -- Genera estímulos de entrada (puedes cambiar estos valores según tus necesidades)
@@ -40,12 +41,22 @@ begin
     --cin <= '0';
 
     -- Proceso de simulación
-    process
+    stim_proc: process
     begin
         -- Inserta aquí tus estímulos de entrada si es necesario
-		a <= "00110011";
-		b <= "11001100";
-		cin <= '0';
+		a_tb <= "00110011";
+		b_tb <= "11001100";
+		cin_tb <= '0';
+		wait for 10 ns;
+
+		a_tb <= "00000111";
+		b_tb <= "11110001";
+		cin_tb <= '0';
+		wait for 10 ns;
+
+		a_tb <= "11111111";
+		b_tb <= "11111111";
+		cin_tb <= '0';
 		wait for 10 ns;
 
 	-- Inserta más estímulos o espera aquí según tus necesidades
