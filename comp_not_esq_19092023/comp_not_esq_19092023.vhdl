0cc2c6252f5287d642666b85e8e3367ceda3f6c9
Hector Manuel Barrios Barrios <hector.barrios@realitat.com.mx>
2023-09-19 16:22:02 -0600

    add esquematico

---
+++ comp_not_esq_19092023/comp_not_esq_19092023.vhdl
@@ -0,0 +1,38 @@
+-- ************************************************************************************************
+-- Development name: Hector Manuel Barrios Barrios                                   
+-- Operating System: GNU/Linux
+                                
+-- Kernel version: 6.3.6-060306-generic
+                                  
+-- ghdl version: -- GHDL 1.0.0 (Ubuntu 1.0.0+dfsg-6) [Dunoon edition]
+--  Compiled with GNAT Version: 10.3.1 20211117
+--  mcode code generator
+-- Written by Tristan Gingold.
+-- 
+-- Copyright (C) 2003 - 2021 Tristan Gingold.
+-- GHDL is free software, covered by the GNU General Public License.  There is NO
+-- warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+                                                           
+-- date create: Tue Sep 19 04:05:32 PM CST 2023
+                                         
+-- ************************************************************************************************
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity comp_not_esq_19092023 is
+        
+       port(
+			a: in std_logic;
+			q: out std_logic
+
+       );
+
+end comp_not_esq_19092023;
+
+architecture comp_not_esq_19092023 of comp_not_esq_19092023 is
+        
+begin
+		q <= not a;
+
+end comp_not_esq_19092023;
---
+++ comp_or_esq_19092023/comp_or_esq_19092023.vhdl
@@ -0,0 +1,39 @@
+-- ************************************************************************************************
+-- Development name: Hector Manuel Barrios Barrios                                   
+-- Operating System: GNU/Linux
+                                
+-- Kernel version: 6.3.6-060306-generic
+                                  
+-- ghdl version: -- GHDL 1.0.0 (Ubuntu 1.0.0+dfsg-6) [Dunoon edition]
+--  Compiled with GNAT Version: 10.3.1 20211117
+--  mcode code generator
+-- Written by Tristan Gingold.
+-- 
+-- Copyright (C) 2003 - 2021 Tristan Gingold.
+-- GHDL is free software, covered by the GNU General Public License.  There is NO
+-- warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+                                                           
+-- date create: Tue Sep 19 03:59:15 PM CST 2023
+                                         
+-- ************************************************************************************************
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity comp_or_esq_19092023 is
+        
+       port(
+			a: in std_logic;
+			b: in std_logic;
+			q: out std_logic
+
+       );
+
+end comp_or_esq_19092023;
+
+architecture comp_or_esq_19092023 of comp_or_esq_19092023 is
+        
+begin
+		q <= a or b;
+
+end comp_or_esq_19092023;
